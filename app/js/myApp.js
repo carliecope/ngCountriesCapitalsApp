d209710284bcc4b6030d87706774407e0c783fab
Carlie Copeland <carliecope@gmail.com>
2016-05-29 21:25:51 -0500

    refactor to move ajax calls to factory

--- app/js/myApp.js
+++ app/js/myApp.js
@@ -1,91 +1,17 @@
-angular.module('myApp', ['ngRoute', 'ngAnimate'])
+angular.module('myApp', ['ngRoute', 'ngAnimate', 'DataServices', 'Data'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl : 'templates/home.html',
-			controller : 'HomeCtrl as home'
+			controller : 'HomeCtrl'
 		}).when('/countries', {
 			templateUrl : 'templates/countries.html',
-			controller : 'CountriesCtrl as countries'
-		}).when('/countries/:country/:capital', {
+			controller : 'CountriesCtrl'
+		}).when('/countries/:countryCode', {
 			templateUrl : 'templates/country.html',
-			controller : 'CountryCtrl' // as country'
+			controller : 'CountryCtrl' 
 		}).otherwise('error', {
 			template: '<p>Error - Page not Found</p>'
 		});
-	}])
-	.factory('currentCountry', ['$cacheFactory', function($cacheFactory) {
-		var cache = $cacheFactory();
-		
-		var put = function(key, value) {
-		
-			cache.put(key, angular.isUndefined(value) ? null : value);
-		};
-		var get = function(key) {
-			return cache.get(key);
-		};
-		
-		return {
-			setCtryPop: function(population) {
-				put('ctryPop', population);
-			},
-			getCtryPop: function() {
-				return get('ctryPop');
-			},
-			setCtryArea: function(area) {
-				put('ctryArea', area);
-			},
-			getCtryArea: function() {
-				return get('ctryArea');
-			},
-			setGeonameId: function(id) {
-				put('geonameId', id);
-			},
-			getGeonameId: function() {
-				return get('geonameId');
-			},
-			setCountry: function(newCountry) {
-				put('country', newCountry);
-			},
-			getCountry: function() {
-				return get('country');
-			},
-			setCapital: function(newCapital) {
-				put('capital', newCapital);
-			},
-			getCapital: function() {
-				return get('capital');
-			},
-			setCode: function(newCode) {
-				put('code', newCode);
-			},
-			getCode: function() {
-				return get('code');
-			},
-			setLowerCaseCode: function(newCode) {
-				put('toLowerCaseCode', newCode);
-			},
-			getLowerCaseCode: function() {
-				return get('toLowerCaseCode');
-			},
-			setCapitalPop: function(newCapitalPop) {
-				put('capitalPop', newCapitalPop);
-			},
-			getCapitalPop: function() {
-				return get('capitalPop');
-			},
-			setNeighbors: function(newNeighbors) {
-				put('neighbors', newNeighbors);
-			},
-			getNeighbors: function() {
-				return get('neighbors');
-			},
-			setNeighborNum: function(newNeighborNum) {
-				put('neighborNum', newNeighborNum);
-			},
-			getNeighborNum: function() {
-				return $scope.get('neighborNum');
-			}
-		};
 	}])
 	.controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {
 
@@ -93,117 +19,49 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
 			$location.path('/countries');
 		};
 	}])
-	.controller('CountriesCtrl', ['$scope', '$http', '$location', 'currentCountry', function($scope, $http, $location, currentCountry) {
+	.controller('CountriesCtrl', ['$scope', '$location', '$q', 'countryData', function($scope, $location, $q, countryData) {
+		'use strict';
 
-		//Get countries list from 'countryInfo' endpoint
-		$http({
-			crossDomain: true,
-			xhrFields: {withCredentials: false},
-			url: 'https://api.geonames.org/countryInfoJSON?username=carliecope/',
-			method: 'JSONP',
-			cache: true
-			}).then(function(response) {
+		//turn objects into strings and store in variable
+  		var toString = Object.prototype.toString;
 
-				var ctryList = response.data.geonames;
+		//Did API call return any data?
+		$q.when(countryData.countries).then(function(result){
 
-				for(i = 0; i < ctryList.length; i++) {
-					if(ctryList[i].capital === "") {
-						ctryList.splice(i, 1);
-					}
-				}
-				$scope.ctryList = ctryList;
-			}, function(response) {
-				console.log('error');
-			});
+			//If we did get data back, is it an object?
+			if(toString.call(countryData.countries)=='[object Object]') {
+			  countryData.countries = result.geonames;
+			}
+			$scope.countries = countryData.countries;
+		});
 
-		$scope.toCountry = function(country, capital, geonameId, population, area, code) {
-			currentCountry.setGeonameId(geonameId);
-			currentCountry.setCtryPop(population);
-			currentCountry.setCtryArea(area);
-			currentCountry.setCountry(country);
-			currentCountry.setCapital(capital);
-			currentCountry.setCode(code);
-			currentCountry.setLowerCaseCode(code.toLowerCase());
+		$scope.toCountry = function(country) {
 
-			$location.path('/countries/' + country + '/' + capital);
+			$location.path('/countries/'+ country.countryCode);
 		}; 
 		$scope.toHome = function() {
 			$location.path('/');
 		};
 	}])
-	.controller('CountryCtrl', ['$scope', '$http', '$routeParams', '$location', 'currentCountry', function($scope, $http, $routeParams, $location, currentCountry) {
-
-		$scope.country = currentCountry.getCountry();
-
-		$scope.capital = currentCountry.getCapital();
-
-		$scope.ctryPop = currentCountry.getCtryPop();
-
-		$scope.ctryArea = currentCountry.getCtryArea();
-
-		$scope.geonameId = currentCountry.getGeonameId();
-
-		$scope.codeUpperCase = currentCountry.getCode();
+	.controller('CountryCtrl', ['$scope', '$location', '$route', 'countryData', function($scope, $location, $route, countryData) {
 		
-		$scope.codeLowerCase = currentCountry.getLowerCaseCode();
+		countryData.getCountry($route.current.params.countryCode).then(function(result){
+    		$scope.country=result[0];
+    		console.log($scope.country);
+ 		});
 
-		//Get capital population from 'search' endpoint
-		var requestSearch = {
-			name_equals: $scope.capital,
-			isNameRequired: true
-		};
-
-		$http({
-			crossDomain: true,
-			xhrFields: {withCredentials: false},
-			url: 'https://api.geonames.org/searchJSON?q=' + $scope.capital + '&featureCode=PPLC&maxRows=10&username=carliecope/',
-			method: 'GET',
-			cache: true,
-			params: requestSearch,
-			}).then(function(response) {
-
-				if(response.data.geonames.length !== 0) {
-					$scope.capitalPop = response.data.geonames[0].population;
-					currentCountry.setCapitalPop($scope.capitalPop);
-
-				} else {
-					$scope.capitalPop = "NA";
-					currentCountry.setCapitalPop($scope.capitalPop);
-				}
-				
-			}, function(response) {
-				console.log('error');
-
-			});
-
-		//Get neighbors from 'neighbors' endpoint 
-		var requestNeighbors = {
-			geonameId: $scope.geonameId
-		};
-		
-		$http({
-			crossDomain: true,
-			xhrFields: {withCredentials: false},
-			url: 'https://api.geonames.org/neighboursJSON?geonameId=' + $scope.geonameId + '&username=carliecope/',
-			method: 'GET',
-			cache: true,
-			params: requestNeighbors,
-		}).then(function(response) {
-			
-			$scope.neighborNum = response.data.geonames.length;
-			currentCountry.setNeighborNum($scope.neighborNum); 
-
-			var neighbors = [];
-
-			for(i=0; i < $scope.neighborNum; i++) {
-				neighbors.push(response.data.geonames[i].countryName);
-				$scope.neighbors = neighbors.join(', ');
-				currentCountry.setNeighbors($scope.neighbors); 
-			}
-		}, function(response) {
-			console.log('error');
+ 		countryData.getCapitals($route.current.params.countryCode).then(function(result){
+		    $scope.capital = result;
+		    $scope.capitalPopulation = $scope.capital.population;
 		});
 
+		countryData.getNeighbors($route.current.params.countryCode).then(function(result){
+			$scope.neighbors = result.geonames;
+		});
+
+		$scope.flag = $route.current.params.countryCode.toLowerCase();
+		$scope.map = $route.current.params.countryCode;
+		
 		$scope.backToCountries = function() {
 			$location.path('/countries');
 		}; 
