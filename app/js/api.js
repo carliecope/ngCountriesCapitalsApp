6127936d46a15c7335046c2b09e6abd9a51defff
Carlie Copeland <carliecope@gmail.com>
2016-05-29 21:42:15 -0500

    add api and data js files

---
+++ app/js/api.js
@@ -0,0 +1,134 @@
+angular.module('DataServices', [])
+	
+	.factory('dataFactory', ['$http', '$q', '$route', function($http, $q, $route) {
+		
+		//User name for geonames api
+		var username = "carliecope";
+
+		var baseURL = "http://api.geonames.org/";
+
+		return {
+
+			//Get countries list call
+
+			getCountries: function() {
+				var defer = $q.defer();
+				var url = baseURL + "countryInfoJSON";
+				var request = { 
+					callback: 'JSON_CALLBACK',
+					username: username
+				};
+
+				$http({
+					method: 'JSONP',
+					url: url,
+					params: request,
+					cache: true
+				})
+				.success(function(data, status, headers, config) {
+			        if(typeof data.status == 'object') {
+			          console.log("Error'" + data.status.message + ".");
+			          defer.reject(data.status);
+			        } else {
+			          defer.resolve(data);
+			        }
+				})
+				.error(function(data, status, headers, config) {
+	        		console.log(status + " error occurred.");
+	        		defer.reject();
+				});
+
+				console.log(defer.promise);
+				return defer.promise;
+		    },
+
+			 //Get capital population from 'search endpoint'
+
+		    getCapitals: function(countryCode) {
+		    	var defer = $q.defer();
+		    	var url = baseURL + 'searchJSON?';
+		    	var request = {
+		    		callback: 'JSON_CALLBACK',
+		    		q: "capital",
+		    		formatted: true,
+		    		country: countryCode,
+		    		maxRows: 1,
+    				username: username
+		    	};
+
+			    $http({
+			    	method: 'JSONP',
+			    	url: url,
+			    	params: request,
+			    	cache: true
+			    })
+			    .success(function(data, status, headers, config) {
+			    	defer.resolve(data.geonames[0]);
+
+			    })
+			    .error(function(data, status, headers, config) {
+	        		console.log(status + " error occurred.");
+	        		defer.reject();
+				});
+
+			    return defer.promise;
+			},
+
+			//Get neighbors from 'neightbors' endpoint
+			getNeighbors: function(countryCode) {
+				var defer = $q.defer();
+				var url = baseURL + 'neighboursJSON';
+				var request = {
+			        callback: 'JSON_CALLBACK',
+			        country: countryCode,
+			        username: username
+				};
+
+				$http({
+					method: 'JSONP',
+					url: url,
+					params: request,
+					cache: true
+				})
+				.success(function(data, status, headers, config) {
+			    	defer.resolve(data);
+
+			    })
+			    .error(function(data, status, headers, config) {
+	        		console.log(status + " error occurred.");
+	        		defer.reject();
+				});
+
+			    return defer.promise;
+			}, 
+			//function to make request for info on a country
+		    getCountry: function(countryCode) {
+				var defer = $q.defer();
+				var url = baseURL + "countryInfoJSON";
+				var request = {
+					callback: 'JSON_CALLBACK',
+					country: countryCode,
+					username: username
+				};
+
+				$http({
+					method: 'JSONP',
+					url: url,
+					params: request,
+					cache: true
+				})
+				.success(function(data, status, headers, config) {
+				defer.resolve(data.geonames);
+				})
+				.error(function(data, status, headers, config) {
+				console.log(status + " error occured.");
+				defer.reject();
+				});
+
+				return defer.promise;
+			}
+		};
+	}]);
+
+
+
---
+++ app/js/data.js
@@ -0,0 +1,13 @@
+//Make data object accessible for all pages of app 
+
+angular.module('Data', [])
+
+.factory('countryData', ['dataFactory', function(dataFactory) {
+  var countryData = {};
+
+  countryData.countries = dataFactory.getCountries();
+  countryData.getCountry = dataFactory.getCountry;
+  countryData.getCapitals = dataFactory.getCapitals;
+  countryData.getNeighbors = dataFactory.getNeighbors;
+  return countryData;
+}]);
